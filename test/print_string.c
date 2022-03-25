84af90cf2de98c0646df664e48bf753e7ca1964c
TeoVH <villadamateo64@gmail.com>
2022-03-24 19:51:20 -0500

    commit

---
+++ test/print_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+char *print_S(va_list list)
+{
+	char *s;
+	char *p;
+	int len;
+
+	s = va_arg(list, char *);
+
+	if (s == NULL)
+	{
+		s = "(null)";
+	}
+
+	len = _strlen(s);
+
+	p = malloc(sizeof(char) * len + 1);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	return (_strcpy(p, s));
+}
